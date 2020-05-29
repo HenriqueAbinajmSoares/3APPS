9f1c4f5dc775a27317efb793bff162784c04689e
HenriqueAbinajmSoares <henrique030103@gmail.com>
2020-05-29 11:46:37 -0300

    ATUALIZAÇÃO
    11:46 - 29/05/2020

--- 3APPS/app/src/main/java/com/example/a3apps/MainActivity.java
+++ 3APPS/app/src/main/java/com/example/a3apps/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.a3apps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnAPP1OnClick(View view) {
+        Intent intentAPP1 = new Intent(MainActivity.this, Main2Activity.class);
+        startActivity(intentAPP1);
+    }
+
+    public void btnAPP2OnClick(View view) {
+        Intent intentAPP2 = new Intent(MainActivity.this, Main3Activity.class);
+        startActivity(intentAPP2);
+    }
+
+    public void btnAPP3OnClick(View view) {
+        Intent intentAPP3 = new Intent(MainActivity.this, Main4Activity.class);
+        startActivity(intentAPP3);
     }
 }
